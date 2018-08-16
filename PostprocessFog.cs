b42328a018a86749b833618fcd5b9f75a2fc3fbf
Yuehai <dreambuffer@163.com>
2018-08-16 16:02:33 +0800

    init

---
+++ PostprocessFog.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostprocessFog : PostEffectBase {
+	[SerializeField] Color fogColor;
+	[SerializeField] float fogStart;
+	[SerializeField, Range(0, 10)] float fogEnd;
+	[SerializeField, Range(0, 1)] float fogDensity;
+
+	private void OnEnable() {
+		Camera.main.depthTextureMode |= DepthTextureMode.Depth;
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material != null)
+        {
+			Matrix4x4 frustumCorners = Matrix4x4.identity;
+			float fov = Camera.main.fieldOfView;
+			float near = Camera.main.nearClipPlane;
+			float far = Camera.main.farClipPlane;
+			float aspect = Camera.main.aspect;
+
+			float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+			Vector3 toRight = Camera.main.transform.right * halfHeight * aspect;
+			Vector3 toTop = Camera.main.transform.up * halfHeight;
+
+			Vector3 topLeft = Camera.main.transform.forward * near + toTop - toRight;
+			float scale = topLeft.magnitude / near;
+
+			topLeft.Normalize();
+			topLeft *= scale;
+
+			Vector3 topRight = Camera.main.transform.forward * near + toRight + toTop;
+			topRight.Normalize();
+			topRight *= scale;
+
+			Vector3 bottomLeft = Camera.main.transform.forward * near - toTop - toRight;
+			bottomLeft.Normalize();
+			bottomLeft *= scale;
+
+			Vector3 bottomRight = Camera.main.transform.forward * near + toRight - toTop;
+			bottomRight.Normalize();
+			bottomRight *= scale;
+
+			frustumCorners.SetRow(0, bottomLeft);
+			frustumCorners.SetRow(1, bottomRight);
+			frustumCorners.SetRow(2, topRight);
+			frustumCorners.SetRow(3, topLeft);
+
+			material.SetMatrix("_FrustumCornersRay", frustumCorners);
+			material.SetMatrix("_ViewProjectionInverseMatrix", (Camera.main.projectionMatrix * Camera.main.worldToCameraMatrix).inverse);
+
+			material.SetColor("_FogColor", fogColor);
+			material.SetFloat("_FogStart", fogStart);
+			material.SetFloat("_FogEnd", fogEnd);
+			material.SetFloat("_FogDensity", fogDensity);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    private void Update()
+    {
+        // blendOpacity = Mathf.Clamp(blendOpacity, 0.0f, 1.0f);
+    }
+
+}
---
+++ fog.shader
@@ -0,0 +1,92 @@
+﻿Shader "dreambuffer/Hidden/fog"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+
+		_FogDensity ("_FogDensity", float) = 1.0
+		_FogColor ("FogColor", Color) = (0.5, 0, 0, 0.5)
+		_FogStart("Fog Start", float) = 0  
+		_FogEnd("Fog End", float) = 300  
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			#include "../../CgIncludes/func.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				float4 pos : SV_POSITION;
+				float2 uv_depth : TEXCOORD1;
+				float4 interpolatedRay : TEXCOORD2;
+			};
+
+			float4x4 _FrustumCornersRay;
+
+
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				o.uv_depth = v.uv;
+
+				#if UNITY_UV_STARTS_AT_TOP
+					if(_MainTex_TexelSize < 0)
+						o.uv_depth.y = 1-o.uv_depth.y;
+				#endif
+
+				int index = 0;
+				if(v.uv.x < 0.5 && v.uv.y < 0.5){
+					index = 0;
+				}else if(v.uv.x > 0.5 && v.uv.y < 0.5){
+					index = 1;
+				}else if(v.uv.x > 0.5 && v.uv.y > 0.5){
+					index = 2;
+				}else{
+					index = 3;
+				}
+
+				#if UNITY_UV_STARTS_AT_TOP
+				if(_MainTex_TexelSize.y < 0)
+					index = 3 - index;
+				#endif
+
+				o.interpolatedRay = _FrustumCornersRay[index];
+
+				return o;
+			}
+			
+			sampler2D _MainTex;
+			half4 _MainTex_TexelSize;
+
+			fixed4 _FogColor;
+
+			fixed4 frag (v2f i) : SV_Target
+			{
+				fixed4 finalColor = tex2D(_MainTex, i.uv);				
+
+				finalColor.rgb = lerp(finalColor.rgb, _FogColor.rgb, fogDensity(i.uv_depth, i.interpolatedRay.xyz));
+
+				return finalColor;
+			}
+			ENDCG
+		}
+	}
+}
